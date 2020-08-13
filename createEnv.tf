52860cca0fe5bd2d51e2fd1ef58a213dfc02a8ff
Omar Saber <osaber@localhost.localdomain>
2020-08-13 11:24:28 +0200

    updating repo

---
+++ createEnv.tf
@@ -0,0 +1,7 @@
+# Terraform file to automate creating Components for the Kubernetes cluster
+
+# Create S3 resource  to hold KOPS_STATE_STORE
+
+# create DNS hosted zone for the cluster to be hosted in
+
+#create security group for the cluster
